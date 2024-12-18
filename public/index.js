7dea75dac52d44acfbbfaf7d75d63944491518df
Glitch (glitch-hello-node) <none>
2024-12-18 17:52:33 +0000

    🐗👐 Checkpoint ./public/style.css:71334325/2527 ./public/index.js:71334325/192 ./public/effective-combos.html:69870694/1970

--- public/index.js
+++ public/index.js
@@ -6,3 +6,10 @@ const app = express();
 const port = 3000;
 
 // Middleware
+
+// script.js
+
+document.getElementById('sidebar-toggle').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('show');
+});

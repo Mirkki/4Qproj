d1a89ac99f1926b2758b18a2e514d8a6044e0cb1
Glitch (glitch-hello-node) <none>
2025-01-31 06:04:00 +0000

    🐃🏫 Checkpoint ./public/index.html:69870694/2165:71334325/1747 ./.glitch-assets:69870694/968 ./public/index.js:69870694/714 ./public/style.css:69870694/595

--- public/index.js
+++ public/index.js
@@ -16,32 +16,3 @@ sidebarToggleBtn.addEventListener("click", () => {
 });
 
 
-let slideIndex = 0;
-showSlides();
-
-// Function to move to the next or previous slide
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Function to display the current slide
-function showSlides(n) {
-    let slides = document.getElementsByClassName("mySlides");
-
-    // Reset slide index if out of bounds
-    if (n >= slides.length) { slideIndex = 0; }
-    if (n < 0) { slideIndex = slides.length - 1; }
-
-    // Hide all slides
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Display the current slide
-    slides[slideIndex].style.display = "block";
-}
-
-// Automatically change slides every 5 seconds
-setInterval(() => {
-    plusSlides(1);
-}, 5000);
